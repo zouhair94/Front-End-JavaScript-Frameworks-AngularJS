08b69b741cd0c5b52cee8f23eae52217c6f2de63
zouhair ridaoui <31044169+zouhair94@users.noreply.github.com>
2018-03-06 12:31:25 +0000

    dish detail modification

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -110,6 +110,91 @@
 
 }])
 
+ .controller('DishDetailController', ['$scope', function($scope) {
+
+            var drating = 5;
+            var dish={
+                          name:'Uthapizza',
+                          image: 'images/uthapizza.png',
+                          category: 'mains', 
+                          label:'Hot',
+                          price:'4.99',
+                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                           comments: [
+                               {
+                                   rating:5,
+                                   comment:"Imagine all the eatables, living in conFusion!",
+                                   author:"John Lemon",
+                                   date:"2012-10-16T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                                   author:"Paul McVites",
+                                   date:"2014-09-05T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:3,
+                                   comment:"Eat it, just eat it!",
+                                   author:"Michael Jaikishan",
+                                   date:"2015-02-13T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Ultimate, Reaching for the stars!",
+                                   author:"Ringo Starry",
+                                   date:"2013-12-02T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:2,
+                                   comment:"It's your birthday, we're gonna party!",
+                                   author:"25 Cent",
+                                   date:"2011-12-02T17:57:28.556094Z"
+                               }
+                               
+                           ]
+                    };
+            
+            $scope.radioData = [
+              { label:'1', value:1 },
+              { label:'2', value:2 },
+              { label:'3', value:3 },
+              { label:'4', value:4 },
+              { label:'5', value:5 ,}
+            ];
+           
+            $scope.addComment =  function(){
+             
+              if($scope.commentForm.$valid)
+              {
+                $scope.dish.comments.push({
+                  rating : $scope.comments.rating ,
+                  comment: $scope.comments.comment ,
+                  author : $scope.comments.author ,
+                  date: $scope.time , 
+                });
+              }
+              $scope.comments.rating = 5 ;
+              $scope.comments.comment = "";
+              $scope.comments.author="";
+              $scope.commentForm.$setPristine();
+              console.log($scope.comments);
+              console.log($scope.dish);
+              console.log($scope.time);
+              console.log($scope.dish.comments[5].date);
+            };
+
+            $scope.dish = dish;
+            $scope.defRad = function(r){
+              if(r==5)
+              {
+                $scope.comments.rating = 5 ;
+              }
+            } ;
+            $scope.time =  Date.now();
+            console.log($scope.dish);
+            
+        }])
 
 .controller('TimeContoller',['$scope','$interval',function($scope,$interval){
   $scope.timeNow = new Date().toUTCString() ;
